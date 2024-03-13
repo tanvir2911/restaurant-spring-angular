ba7e3036f33f3a0674a5f13a7d3caeb2e308bb77
Md Mozammel Hossain <hossainmozammel27@gmail.com>
2024-03-13 16:29:36 +0600

    jasper added

--- backend/restaurant-project/src/main/java/com/restaurant/spring/restaurantproject/controller/FoodItemsController.java
+++ backend/restaurant-project/src/main/java/com/restaurant/spring/restaurantproject/controller/FoodItemsController.java
@@ -4,11 +4,20 @@ package com.restaurant.spring.restaurantproject.controller;
 import com.restaurant.spring.restaurantproject.entity.FoodItems;
 import com.restaurant.spring.restaurantproject.entity.Inventory;
 import com.restaurant.spring.restaurantproject.service.FoodItemsService;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/foodItems")
@@ -42,8 +51,39 @@ public class FoodItemsController {
 
 
 
-    @GetMapping("/search")
-    public ResponseEntity<List<FoodItems>> searchProducts(@RequestParam("query") String query){
-        return ResponseEntity.ok(foodItemsService.searchFoodItems(query));
+    @GetMapping(value = "/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> downloadInvoice() throws JRException, IOException {
+
+        JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource((
+
+//                new Product(121, "Keyboard", 54884),
+//                new Product(122, "Mouse", 54884),
+//                new Product(123, "Laptop", 54884),
+//                new Product(124, "Mobile", 54884),
+//                new Product(125, "Headphone", 54884)
+                foodItemsService.getAll()
+
+        ), false);
+
+        Map<String, Object> parameters = new HashMap<>();
+//        parameters.put("total", "7000");
+
+        JasperReport compileReport = JasperCompileManager
+                .compileReport(new FileInputStream("src/main/resources/foodItems.jrxml"));
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(compileReport, parameters, beanCollectionDataSource);
+
+        // JasperExportManager.exportReportToPdfFile(jasperPrint,
+        // System.currentTimeMillis() + ".pdf");
+
+        byte data[] = JasperExportManager.exportReportToPdf(jasperPrint);
+
+        System.err.println(data);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=citiesreport.pdf");
+
+        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(data);
     }
+
 }
